0eeab0caedea6a29d50f99bea3e82136dd530143
soundaryapurushothaman <143268635+soundaryapurushothaman@users.noreply.github.com>
2025-06-05 15:48:40 +0530

    Create diagonalTraverse.java

---
+++ diagonalTraverse.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] findDiagonalOrder(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[] res = new int[rows * cols];
+        int r = 0;
+        int c = 0;
+        for (int i = 0; i < rows * cols; i++) {
+          res[i] = matrix[r][c];
+          if ((r + c) % 2 == 0) {
+            if (r - 1 >= 0 && c + 1 < cols) {
+              c = c + 1;
+              r = r - 1;
+            } else if (r - 1 < 0 && c + 1 < cols) {
+              c = c + 1;
+            } else if (r + 1 < rows && c + 1 > cols - 1) {
+              r = r + 1;
+            }
+          }
+          else if ((r + c) % 2 != 0) {
+            if (r + 1 < rows && c - 1 >= 0) {
+              c = c - 1;
+              r = r + 1;
+            } else if (r + 1 < rows && c - 1 < 0) {
+              r = r + 1;
+            } else if (r + 1 > rows - 1 && c +1 < cols) {
+              c = c + 1;
+            }
+          }
+        }
+        return res;
+    }
+    }
